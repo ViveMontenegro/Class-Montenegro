89a7b7512febb77204ccaedf5e132eae14c8eb1a
ViveMontenegro <sebastian.monterroso@yahoo.com>
2025-01-24 13:13:02 -0500

    Update Main.java

--- src/main/java/org/csh/Main.java
+++ src/main/java/org/csh/Main.java
@@ -2,12 +2,12 @@ package org.csh;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.print("\n\n");
         int joe = 6;
         int biden = 9;
         int baldBiden = (((joe + biden) + (joe * biden)) * joe) + joe;
         System.out.print(baldBiden);
         
-        
+        System.out.print("\n\n");
     }
 }
