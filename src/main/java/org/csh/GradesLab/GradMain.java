6728e765e92431d6958067a6163d2fc152aea7d8
ViveMontenegro <sebastian.monterroso@yahoo.com>
2025-02-08 15:02:32 -0500

    Create GradMain.java

---
+++ src/main/java/org/csh/GradesLab/GradMain.java
@@ -0,0 +1,13 @@
+package org.csh;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\n\n");
+        Grades mulada = new Grades();
+        mulada.createList();
+
+        System.out.print(mulada);
+        System.out.print("\n\n");
+    }
+
+}
